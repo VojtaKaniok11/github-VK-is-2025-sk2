a1a7816010f8a69a734a350aeb26c91893dde8fa
Vojtěch Kaniok <vojtech.kaniok@tul.cz>
2025-10-10 15:38:37 +0200

    program000a-zakladni-kod

--- IS-Programy/program001-vypis-rady/obj/Debug/net9.0/program001-vypis-rady.AssemblyInfo.cs
+++ IS-Programy/program001-vypis-rady/obj/Debug/net9.0/program001-vypis-rady.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aec1631b9c49b0255d2af3060ed566e8b942dc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5f68714e255e4d8738f782c769ed3041e436efa")]
 [assembly: System.Reflection.AssemblyProductAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
