6e58d89f1e74e869024e7bf7ebd18e1fc4ad4595
Vojtěch Kaniok <vojtech.kaniok@tul.cz>
2025-10-23 14:09:24 +0200

    #8 program004a-obdelnik

--- IS-Programy/program004a-obdelnik/Program.cs
+++ IS-Programy/program004a-obdelnik/Program.cs
@@ -1,62 +1,43 @@
-﻿using System.Runtime.InteropServices.Marshalling;
-
-string again = "a";
+﻿string again = "a";
 while (again == "a")
 {
     Console.Clear();
     Console.WriteLine("***************************************");
-    Console.WriteLine("**********Výpis číselné řady * **********");
+    Console.WriteLine("**********Výpis číselné řady * ********");
     Console.WriteLine("***************************************");
     Console.WriteLine("***************************************");
-    Console.WriteLine("**********Výpis číselné řady ************");
-    Console.WriteLine("**************2.10. 2025 * **************");
+    Console.WriteLine("**********Výpis číselné řady **********");
+    Console.WriteLine("**************2.10. 2025 * ************");
     Console.WriteLine("***************************************");
     Console.WriteLine("***************************************");
-
-    Console.Write("Zadej celé číslo A:");
-    int a;
-    while (!int.TryParse(Console.ReadLine(), out a))
-    {
-        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo znovu A:");
-    }
-
-    Console.Write("Zadej celé číslo B:");
-    int b;
-    while (!int.TryParse(Console.ReadLine(), out b))
-    {
-        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo znovu B:");
-    }
-
-    Console.Write("Zadej celé číslo C:");
-    int c;
-    while (!int.TryParse(Console.ReadLine(), out c))
-    {
-        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo znovu C:");
-    }
-
     Console.WriteLine();
 
-
-    //Tady chceme seřadit čísla vzestupně
-    if (a > b)
+    Console.Write("Zadejte šířku obdélníku: ");
+    int width;
+    while (!int.TryParse(Console.ReadLine(), out width))
     {
-        if (a > c)
-            Console.WriteLine($"Největší číslo je A = {a}");
-        else
-            Console.WriteLine($"Největší číslo je C = {c}");
-    }
-    else
-    {
-        if (b > c)
-            Console.WriteLine($"Největší číslo je B = {b}");
-        else
-            Console.WriteLine($"Největší číslo je C = {c}");
+        Console.WriteLine("Nezadali jste celé číslo. Zadejte šířku obdélníku znovu: ");
     }
 
+    Console.Write("Zadejte výšku obdélníku: ");
+    int height;
+    while (!int.TryParse(Console.ReadLine(), out height))
+    {
+        Console.WriteLine("Nezadali jste celé číslo. Zadejte výšku obdélníku znovu: ");
+    }
+
+
+    for (int i = 1; i <= height; i++)
+        for (int j = 1; j <= width; j++)
+        {
+            Console.Write("*")
+            System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(500));
+        }
+    Console.WriteLine();
 
 
 
     Console.WriteLine();
-    Console.Write("Pro opakování programu stiskněte klávesu a = ano / n = ne");
+    Console.Write("Pro opakování programu stiskněte klávesu a ");
     again = Console.ReadLine();
 }
